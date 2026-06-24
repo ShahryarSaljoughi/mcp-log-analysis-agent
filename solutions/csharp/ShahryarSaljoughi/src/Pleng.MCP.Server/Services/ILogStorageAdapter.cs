6f596fefc01bfa321e3d10b2823a2a9f9175e390
shahryar saljoughi <s.shahryar75@gmail.com>
2026-06-24 16:02:24 +0330

    add logexplorer service and GetLogs MCP tool

---
+++ solutions/csharp/ShahryarSaljoughi/src/Pleng.MCP.Server/Services/ILogStorageAdapter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pleng.MCP.Server.Services;
+
+internal interface ILogStorageAdapter
+{
+    public LogModel[] GetLogs(string serviceName, DateTimeOffset since, DateTimeOffset until);
+}
+
+
+internal class LogModel
+{
+    public LogLevel LogLevel { get; set; }
+    public required string ServiceName { get; set; }
+    public required string ErrorType { get; set; }
+    public long? TimeoutDuration { get; set; }
+    public string? TargetServer { get; set; }
+    public Dictionary<string, object> Properties { get; set; } = new();
+
+    public override string ToString()
+    {
+        return $"[{LogLevel}] - {ServiceName} - {ErrorType} after {TimeoutDuration}ms on SQL-Server-01";
+    }
+}
+
---
+++ solutions/csharp/ShahryarSaljoughi/src/Pleng.MCP.Server/Tools/LogExplorerTools.cs
@@ -0,0 +1,30 @@
+﻿using ModelContextProtocol.Server;
+using Pleng.MCP.Server.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Linq;
+
+namespace Pleng.MCP.Server.Tools;
+
+internal class LogExplorerTools
+{
+    private readonly ILogStorageAdapter _logStorageAdapter;
+
+    public LogExplorerTools(ILogStorageAdapter logAdapter)
+    {
+        this._logStorageAdapter = logAdapter;
+    }
+
+    [McpServerTool(Destructive = false, Idempotent = true, OutputSchemaType = typeof(string[]))]
+    [Description("Returns application logs for a specified service since some minutes ago.")]
+    public async Task<string[]> GetLogsAsync(
+        [Description("name of the service whose logs are requested")] string serviceName, 
+        [Description("number of minutes ago, to start fetch logs")] int minutesAgo)
+    {
+        var start = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromMinutes(minutesAgo));
+        var logs = _logStorageAdapter.GetLogs(serviceName, since: start, DateTimeOffset.UtcNow);
+        return logs is {  } ? [.. logs.Select(l => l.ToString())] : [];
+    }
+}
